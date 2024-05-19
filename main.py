ebf09f609d6c326b95d66870a099227121ed1cfc
Morari Nicolae <42006679+morarinicolae@users.noreply.github.com>
2024-05-19 21:08:02 +0300

    delete Token

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from app.database.models import async_main
 
 async def main():
     await async_main()
-    bot = Bot(token='6545918505:AAE856yJouqGAsqwwhF8IUdFH24cbHwngAc')
+    bot = Bot(token='Token')
     dispatcher = Dispatcher()
     dispatcher.include_router(router)
     await dispatcher.start_polling(bot)
