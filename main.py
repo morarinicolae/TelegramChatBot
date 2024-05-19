a949003e073174fd71df275555b92879a78744f4
Nicolae <morari.nicolae1998@gmail.com>
2024-05-19 21:13:40 +0300

    v1.2 hide TOKEN

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import asyncio
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher, types
+
+import os
+from dotenv import load_dotenv
 
 from app.handlers import router
 from app.database.models import async_main
 
 
-
 async def main():
+    load_dotenv(dotenv_path='hide.env')
+    telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+
+    if not telegram_bot_token:
+        raise ValueError("Nu s-a gasit Tokenul necesar")
+
     await async_main()
-    bot = Bot(token='6545918505:AAE856yJouqGAsqwwhF8IUdFH24cbHwngAc')
+    bot = Bot(token=telegram_bot_token)
     dispatcher = Dispatcher()
     dispatcher.include_router(router)
     await dispatcher.start_polling(bot)
+    
     
 
 
